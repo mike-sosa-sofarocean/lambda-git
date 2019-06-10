dabd46ed4f3588927187b785ebd676a612bae7af
Mike Sosa <mike.sosa@sofarocean.com>
2019-06-10 13:26:19 -0700

    Add option for custom prompted inputs

--- git/__init__.py
+++ git/__init__.py
@@ -34,14 +34,67 @@ if not find_executable('git'):
     os.environ['LD_LIBRARY_PATH'] = LD_LIBRARY_PATH
 
 
+# def exec_command(*args, **kwargs):
+#     ''' For standard commands without any command line input '''
+#     options = dict({'cwd': '/tmp', 'env': os.environ}, **kwargs)
+#     command = ['git'] + list(args)
+#     LOGGER.info('executing git command: "{}"'.format(' '.join(command)))
+#     p = subprocess.Popen(command, stdout=subprocess.PIPE,
+#                          stderr=subprocess.PIPE, cwd=options['cwd'],
+#                          env=options['env'])
+#     stdout, stderr = p.communicate()
+#     if p.returncode != 0:
+#         LOGGER.error('git failed with {} returncode'.format(p.returncode))
+#         raise GitExecutionError(
+#             'command={} returncode={} stdout="{}" '
+#             'stderr="{}"'.format(command, p.returncode, stdout, stderr)
+#         )
+#     return stdout, stderr
+
 def exec_command(*args, **kwargs):
+    ''' 
+    For commands when command line input is needed add a keyworded arg named 'clinput'
+    which is a list of the things you need to input in order of how they are asked. 
+    For example, if your repository is private, and requires a username/password.
+
+    Example 1:
+        You want to clone a private repo. You can clone via 
+            git clone [user]:[password]@host.xz/path/to/repo.git
+        However this will leave the password in your git/bash history, and also 
+        is not allowed if you use an access token as your password (via github standards).
+        So instead you can 
+            git clone host.xz/path/to/repo.git
+        Then your keywords arg should include
+            git.exec_command('clone', 'pathtorepo', ..., clinput=['username', 'password'])
+        
+        Or you could
+            git clone [user@]host.xz/path/to/repo.git
+        In which your keywords arg should inlude
+            git.exec_command('clone', 'pathtorepo', ..., clinput=['password'])
+    Example 2:
+        Clone using an access token. 
+            git clone [user]@host.xz:path/to/repo.git
+        In which it will prompt for a password which is passed in.
+            git.exec_command('clone', 'pathtorepo', ..., clinput=['accesstoken'])
+    '''
     options = dict({'cwd': '/tmp', 'env': os.environ}, **kwargs)
     command = ['git'] + list(args)
     LOGGER.info('executing git command: "{}"'.format(' '.join(command)))
-    p = subprocess.Popen(command, stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE, cwd=options['cwd'],
-                         env=options['env'])
-    stdout, stderr = p.communicate()
+
+    p, stdout, stderr = None, None, None
+
+    if 'clinput' in options:
+        p = subprocess.Popen(command, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE, cwd=options['cwd'],
+                             env=options['env'], universal_newlines=True)
+        newline = os.linesep
+        stdout, stderr = p.communicate( newline.join(options['clinput']) )
+    else:
+        p = subprocess.Popen(command, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE, cwd=options['cwd'],
+                             env=options['env'])
+        stdout, stderr = p.communicate()
+    
     if p.returncode != 0:
         LOGGER.error('git failed with {} returncode'.format(p.returncode))
         raise GitExecutionError(
@@ -49,3 +102,4 @@ def exec_command(*args, **kwargs):
             'stderr="{}"'.format(command, p.returncode, stdout, stderr)
         )
     return stdout, stderr
+        
